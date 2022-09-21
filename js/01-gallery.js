920ca7871bacf27b2d55c2053da0f77e177e53fd
IrynaFetisova <109384375+IrynaFetisova@users.noreply.github.com>
2022-09-21 20:01:04 +0300

    hw - js2

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryRef = document.querySelector('.gallery');
+const galleryPic = [...galleryItems].map(({preview, original, description}) => `<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a>`);
+
+const markup = galleryPic.join('');
+galleryRef.insertAdjacentHTML('beforeend', markup);
+console.log(galleryItems);
+
+
+galleryRef.addEventListener("click", showPicture);
+
+function showPicture (event) {
+    blockStandartAction(event);
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+    instance.show();
+
+    galleryRef.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    });
+};
+
+function blockStandartAction(event) {
+    event.preventDefault()
+};
