33342dd53048b631eb47b348d7374e2cbc134a0a
IrynaFetisova <109384375+IrynaFetisova@users.noreply.github.com>
2022-09-21 22:37:48 +0300

    hw-2js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+const galleryPic = [...galleryItems].map(({preview, original, description}) => `<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}"
+/>
+</a>`);
+
+const markup = galleryPic.join('');
+galleryRef.insertAdjacentHTML('beforeend', markup);
 console.log(galleryItems);
+
+galleryRef.addEventListener("click", showPicture);
+
+function showPicture (event) {
+    blockStandartAction(event);
+
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt',
+    captionDelay: 250,});
+}; 
+
+function blockStandartAction(event) {
+    event.preventDefault()
+};
+
